4ed280f0e81427c86663bd3b639915c4471949d7
Luizino <luis.luizino@gmail.com>
2023-03-30 15:12:44 -0300

    implementando busca

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+import "./css/MovieGrid.css"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
 const Search = () => {
-    return <div>Search</div>
+    const [searchParams] = useSearchParams()
+    const [movies, setMovies] = useState ([])
+    const query = searchParams.get("q")
+
+    return (
+        <div className="container">
+            <h2 className="title">
+                Resultados: <span className="query-text">{query}</span>
+            </h2>
+            <div className="movies-container">
+                {movies.length === 0 && <p>Carregando....</p>}
+                {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
+    )
 };
 
 export default Search;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
 import "./css/Navbar.css"
 
 const Navbar = () => {
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!search) return
+        navigate(`/search?q=${search}`)
+        setSearch("")
+    }
     return (
         <nav id="navbar">
             <h2>
@@ -10,8 +19,8 @@ const Navbar = () => {
                     <BiCameraMovie /> Filmes
                 </Link>
             </h2>
-            <form>
-                <input type="test" placeholder="Busque um filme" />
+            <form onSubmit={handleSubmit}>
+                <input type="test" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} value={search} />
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
