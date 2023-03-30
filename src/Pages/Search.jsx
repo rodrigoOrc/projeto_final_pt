8e522b835bf7e8b756aabc5bc411451fdb5b3ace
Luizino <luis.luizino@gmail.com>
2023-03-30 16:22:19 -0300

    formatando

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -5,6 +5,7 @@ import "./css/MovieGrid.css"
 
 const searchURL = import.meta.env.VITE_SEARCH
 const apiKey = import.meta.env.VITE_API_KEY
+
 const Search = () => {
     const [searchParams] = useSearchParams()
   
@@ -28,7 +29,6 @@ const Search = () => {
                 Resultados: <span className="query-text">{query}</span>
             </h2>
             <div className="movies-container">
-                {movies.length === 0 && <p>Carregando....</p>}
                 {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
             </div>
         </div>
