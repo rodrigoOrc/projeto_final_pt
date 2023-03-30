543e54a6d95452aaf5deea9c772c89aca345b7e6
Luizino <luis.luizino@gmail.com>
2023-03-30 11:51:34 -0300

    adicionando link para as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">Filmes</Link>
+        </h2>
+        <Link to="movie/1">Filmes</Link>
+        <Link to="/search">Search</Link>
+      </nav>
       <h2>Filmes</h2>
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Home from './Pages/Home'
+import Movie from './Pages/Movie'
+import Search from './Pages/Search'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<Home />} />
+          <Route path="/movie/:id" element={<Movie />} />
           <Route path="search" element={<Search />} />
         </Route>
       </Routes>
