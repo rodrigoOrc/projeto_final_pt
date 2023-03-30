c802fba05272d755af549d008e32e289556895de
Luizino <luis.luizino@gmail.com>
2023-03-30 15:50:26 -0300

    tentativa fracassada de estabelecer busca

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -7,8 +7,20 @@ const searchURL = import.meta.env.VITE_SEARCH
 const apiKey = import.meta.env.VITE_API_KEY
 const Search = () => {
     const [searchParams] = useSearchParams()
-    const [movies, setMovies] = useState ([])
+  
+    const [movies, setMovies] = useState([])
     const query = searchParams.get("q")
+  
+    const getSearchedMovies = async (url) => {
+      const res = await fetch(url)
+      const data = await res.json()
+      setMovies(data.results)
+    };
+  
+    useEffect(() => {
+      const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+      getSearchedMovies(searchWithQueryURL)
+    }, [query])
 
     return (
         <div className="container">
