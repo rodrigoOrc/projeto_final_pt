5b40f1b2382b1d59b17d14fdd227c891ced19f8e
Davi Mesquita <davimesquita.2004@gmail.com>
2023-03-31 21:51:57 -0300

    css-atualizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <nav id="navbar">
             <h2>
                 <Link to="/">
-                    <BiCameraMovie /> Filmes
+                    <BiCameraMovie /> Purple Filmes
                 </Link>
             </h2>
             <form onSubmit={handleSubmit}>
